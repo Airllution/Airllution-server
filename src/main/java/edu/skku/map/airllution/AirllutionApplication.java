a59978c06baaa47d5b6849a15faf1c11e3983deb
Min-Ho CHO <chominho96@g.skku.edu>
2023-05-30 15:46:37 +0900

    init: project init

---
+++ src/main/java/edu/skku/map/airllution/AirllutionApplication.java
@@ -0,0 +1,13 @@
+package edu.skku.map.airllution;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirllutionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirllutionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/skku/map/airllution/AirllutionApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.skku.map.airllution;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirllutionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
